2e61082c44cdc2fd9da1841a0ec92d62d82296ba
IJM <ivan.jaraiz@gmail.com>
2020-07-05 14:31:02 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operators_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            Console.WriteLine( a + b);
+            Console.WriteLine((float)a / (float)b); // need to cast conversion
+            Console.WriteLine(a + b * c);
+            Console.WriteLine( a > b ); // return bool 
+            Console.WriteLine( a == b); // a != b.. 
+            Console.WriteLine(!(a != b)); //
+            Console.WriteLine( c > b && c > a);
+            Console.WriteLine( c > b && c == a);
+
+        }
+    }
+}
